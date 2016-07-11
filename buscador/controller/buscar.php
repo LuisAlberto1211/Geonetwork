56065155081692bf145e17ddb112f83f288abcd6
Luis Alberto Ortega <mew_9010@hotmail.com>
2016-07-11 01:27:22 -0500

    Modificado el modo de parsear el xml y obtener los datos de uuid y title

--- buscador/controller/buscar.php
+++ buscador/controller/buscar.php
@@ -1,83 +1,29 @@
-<meta charset="utf-8">
-<?php if (isset($_GET['bootstrap']) && $_GET['bootstrap'] == 1):?>
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
-<?php else:?>
-
-<?php endif?>
-<link rel="stylesheet" href="style.css" type="text/css">
 <?php
-    //include 'PaginationPrototype.php';    
-    $xml=simplexml_load_file("http://smit.cenapred.gob.mx:8080/geonetwork/srv/eng/rss.search?any=".$_POST["nombre"]) or die("Error: Cannot create object");
-    //var_dump($xml);
-    //var_dump($xml->item[0]);
-    //echo $xml->item->title.'//<br>';
-    for($i= 0; $i< count($xml);$i++){
-        //echo $xml->attributes->title.'//<br>';
+  $base_url = "http://smit.cenapred.gob.mx:8080/geonetwork/srv/eng/";
+  $search_elements_url  = $base_url."rss.search?any=";
+  $search_element_url   = $base_url."csw?request=GetRecordById&service=CSW&version=2.0.2&elementSetName=full&id=";
+  $search_metadata_url  = $base_url."xml.metadata.get?uuid=";
+
+  if(isset($_GET["rss"])){
+    $xml = simplexml_load_file($search_elements_url."VOLCAN");
+    $response = Array();
+
+    foreach ($xml->channel->item as $item) {
+      $uuidArray = explode("=", $item->guid);
+      $data["uuid"] = $uuidArray[1];
+      $data["title"] = (string)$item->title;
+      $response[] = $data;
     }
+    echo json_encode($response);
 
-    foreach( $xml->children() AS $child )
-    {
-        //run any query you want on the children.. they are also nodes.
-        $name = $child->getName();
-        //echo $name.'//';
+  } else if(isset($_GET["uuid"])){
+    if(isset($_GET["element_info"])){
+
+    } else if(isset($_GET["metadata"])){
+
     }
+  } else{
 
-    //$simple = simplexml_load_string($xml);
+  }
 
-    $arr = json_decode( json_encode($xml) , 1);
-    // var_dump($arr);
-    //print_r($arr);
-    $array = array();
-    $array2 = array();
-    $i =0;
-    $j =0;
-    $uudid =array();
-    foreach( $arr AS $child ){
-        foreach( $child AS $child2 => $valor  ){
-            if($child2 == 'item'){
-                //$array= $child['item'];
-                //var_dump($child['item']);
-                //break;
-                foreach( $child['item'] AS $child3 => $valor3  ){
-                    if($child3 >= 1){
-                        foreach( $child['item'][$child3] AS $child4 => $valor4  ){
-                            if($child4 == 'guid'){
-                                $array2 = explode("=",$valor4);
-                                if (isset($array2[1])) {
-                                    $uudid[$j] = $array2[1];
-                                    $j++;
-                                }
-                            }
-                            if($child4 == 'title'){
-
-                                $array[$i]['title'] = $valor4;
-                                //$array[$i]['uudid'] = $uudid;
-                                $i++;
-                            }
-                            
-                        }
-                        
-                    }
-                    
-                }
-            }
-        }
-    }
-
-    for ($i=0; $i <count($uudid) ; $i++) { 
-        $array[$i]['uudid'] = $uudid[$i];
-    }
-    //var_dump($array);
-
-    $table = '<table>';
-    for ($i=0; $i < count($array); $i++) { 
-        $table .= '<tr>';
-            $table .= '<td>';
-                $table .= '<span onclick="jsDetalle(\''.$array[$i]['uudid'].'\')" style="cursor:pointer">'.($i+1).'.- '.$array[$i]['title'].'</span>';
-            $table .= '</td>';
-        $table .= '</tr>';    
-    }
-    $table .= '</table>';
-    echo $table;
-
-
+?>
