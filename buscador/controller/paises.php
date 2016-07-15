3488e708004a6b9190c6636976e2648ca49dfbea
Luis Alberto Ortega <mew_9010@hotmail.com>
2016-07-14 23:56:08 -0500

    Lista de países agregadas con campos nombre(completo), clave(tomado del COI), url asociada a servicio de geonetwork

---
+++ buscador/controller/paises.php
@@ -0,0 +1,12 @@
+<?php
+  $paises = Array();
+  $paises[]  = [  "nombre"  =>  "El Salvador",
+                  "clave"   =>  "ESA",
+                  "url"     =>  "http://190.86.209.130:8080/geonetwork/srv/eng/"];
+  $paises[]  = [  "nombre"  =>  "Honduras",
+                  "clave"   =>  "HON",
+                  "url"     =>  "http://190.5.105.59:8080/geonetwork/srv/eng/"];
+  $paises[]  = [  "nombre"  =>  "México",
+                  "clave"   =>  "MEX",
+                  "url"     =>  "http://smit.cenapred.gob.mx:8080/geonetwork/srv/eng/"];
+?>
