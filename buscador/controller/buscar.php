cf54acc22250b773f07f2ce04f4aed13bcef3cd1
Luis Alberto Ortega <mew_9010@hotmail.com>
2016-07-14 23:58:53 -0500

    Se hace uso de la lista de países para hacer las búsquedas en su respectiva url. Se creó una función para devolver la lista de países para poder ser mostradas en la opción de selección del formulario de la página principal. Se verifica si el número de resultados de la búsqueda es mayor a cero para optimiza la respuesta del servidor

--- buscador/controller/buscar.php
+++ buscador/controller/buscar.php
@@ -1,14 +1,16 @@
 <?php
+  require "paises.php";
   $resultsPerPage = 10;
-  $base_url = "http://smit.cenapred.gob.mx:8080/geonetwork/srv/eng/";
-  $search_elements_url  = $base_url."xml.search?any=";
-  $search_element_url   = $base_url."csw?request=GetRecordById&service=CSW&version=2.0.2&elementSetName=full&id=";
-  $search_metadata_url  = $base_url."xml.metadata.get?uuid=";
+  $search_elements_url  = "xml.search?any=";
+  $search_element_url   = "csw?request=GetRecordById&service=CSW&version=2.0.2&elementSetName=full&id=";
+  $search_metadata_url  = "xml.metadata.get?uuid=";
 
-  if(isset($_GET["search"]) && isset($_GET["text"])){
+  if(isset($_GET["search"]) && isset($_GET["text"]) && isset($_GET["country"])){
     $page = isset($_GET["page"]) ? $_GET["page"]: 1;
     $text = $_GET["text"];
-    $xmlStr = file_get_contents($search_elements_url.$text);
+    $country = $_GET["country"];
+    $base_url = $paises[$country]["url"];
+    $xmlStr = file_get_contents($base_url.$search_elements_url.$text);
     $p = xml_parser_create();
     xml_parse_into_struct($p, $xmlStr, $xml_arr, $xml_index);
     xml_parser_free($p);
@@ -19,24 +21,31 @@
     $maxPages = ceil($response["length"]/$resultsPerPage);
     $page = ($page > $maxPages) ? $maxPages : $page;
     $page = ($page < 1) ? 1 : $page;
+    $page = ($response["length"] == 0) ? 0 : $page;
     $response["pages"] = $maxPages;
     $response["page"] = (int)$page;
 
-    $inicio = $resultsPerPage*($page-1);
-    $fin = $inicio;
-    if(($response["length"]-($resultsPerPage*($page-1))) >= $resultsPerPage){
-      $fin += $resultsPerPage;
-    } else if($response["length"] < $resultsPerPage){
-      $fin += $response["length"];
-    } else {
-      $fin += $response["length"]%($resultsPerPage*($page-1));
+    if($response["length"] != 0){
+      $inicio = $resultsPerPage*($page-1);
+      $fin = $inicio;
+      if(($response["length"]-($resultsPerPage*($page-1))) >= $resultsPerPage){
+        $fin += $resultsPerPage;
+      } else if($response["length"] < $resultsPerPage){
+        $fin += $response["length"];
+      } else {
+        $fin += $response["length"]%($resultsPerPage*($page-1));
+      }
+
+      for($i = $inicio; $i < $fin; $i++){
+        $uuids[] = $xml_arr[$xml_index["GEONET:INFO"][$i*2]+2]['value'];
+      }
+
+      $response["metadata"] = getDataFromUUID($uuids, $base_url, $search_element_url, $search_metadata_url);
+    }
+    else{
+      $response["metadata"] = [];
     }
 
-    for($i = $inicio; $i < $fin; $i++){
-      $uuids[] = $xml_arr[$xml_index["GEONET:INFO"][$i*2]+2]['value'];
-    }
-
-    $response["metadata"] = getDataFromUUID($uuids, $base_url, $search_element_url, $search_metadata_url);
     echo json_encode($response);
 
   } else if(isset($_GET["uuid"])){
@@ -45,8 +54,8 @@
     } else if(isset($_GET["metadata"])){
 
     }
-  } else{
-
+  } else if(isset($_GET["countries"])){
+    echo json_encode($paises);
   }
 
   function getDataFromUUID($uuids_array, $base_url, $search_element_url, $search_metadata_url){
@@ -54,7 +63,7 @@
     $data = Array();
     $contact = Array();
     foreach ($uuids_array as $uuid) {
-      $xmlStr = file_get_contents($search_element_url.$uuid);
+      $xmlStr = file_get_contents($base_url.$search_element_url.$uuid);
       $p = xml_parser_create();
       xml_parse_into_struct($p, $xmlStr, $xml_arr, $xml_index);
       xml_parser_free($p);
@@ -63,7 +72,7 @@
       $data_arr["abstract"] = $xml_arr[$xml_index["DCT:ABSTRACT"][0]]['value'];
       $data_arr["image_url"] = $base_url.$xml_arr[end($xml_index["DC:URI"])]['value'];
 
-      $xmlStr = file_get_contents($search_metadata_url.$uuid);
+      $xmlStr = file_get_contents($base_url.$search_metadata_url.$uuid);
       $p = xml_parser_create();
       xml_parse_into_struct($p, $xmlStr, $xml_arr, $xml_index);
       xml_parser_free($p);
