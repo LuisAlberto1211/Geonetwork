541edda0a8600bf70e2bbe179dca517794850d1c
Luis Alberto Ortega <mew_9010@hotmail.com>
2016-07-15 20:03:21 -0500

    Cambios en la forma de guardar la información de cada país

--- buscador/controller/paises.php
+++ buscador/controller/paises.php
@@ -1,12 +1,14 @@
 <?php
   $paises = Array();
   $paises[]  = [  "nombre"  =>  "El Salvador",
-                  "clave"   =>  "ESA",
-                  "url"     =>  "http://190.86.209.130:8080/geonetwork/srv/eng/"];
+                  "clave"   =>  "ESA"];
   $paises[]  = [  "nombre"  =>  "Honduras",
-                  "clave"   =>  "HON",
-                  "url"     =>  "http://190.5.105.59:8080/geonetwork/srv/eng/"];
+                  "clave"   =>  "HON"];
   $paises[]  = [  "nombre"  =>  "México",
-                  "clave"   =>  "MEX",
-                  "url"     =>  "http://smit.cenapred.gob.mx:8080/geonetwork/srv/eng/"];
+                  "clave"   =>  "MEX"];
+
+  $url_geonetwork = Array();
+  $url_geonetwork["ESA"] = ["url" => "http://190.86.209.130:8080/geonetwork/srv/eng/"];
+  $url_geonetwork["HON"] = ["url" => "http://190.5.105.59:8080/geonetwork/srv/eng/"];
+  $url_geonetwork["MEX"] = ["url" => "http://smit.cenapred.gob.mx:8080/geonetwork/srv/eng/"];
 ?>
